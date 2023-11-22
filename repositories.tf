a6ae2ff80a3f377bd779362f161bcfb4ac2777b7
Liam MacPherson <liam.macpherson@digital.homeoffice.gov.uk>
2023-11-22 16:08:06 +0000

    LZA-42: add default repository data settings
    This change adds the pre-described repository settings for each
    repository. Due to a limitation within Terraform with being unable to
    loop around a tuple, the variable has been changed to a local with an
    object for each repository under `repository_config`.
    
    There is also an additional object that runs a foreach loop around the
    repository config and inserts specific attributes if they do not
    already exist. It also ensures that the visibility is set to private by
    default unless another is opted into; ensuring that less secure options
    have to be opted into.

---
+++ repositories.tf
@@ -0,0 +1,33 @@
+locals {
+  repository_config = {
+    "core-cloud" = {
+      visibility = "public"
+    },
+    "core-cloud-lza-config" = {
+      visibility = "internal"
+    },
+    "core-cloud-github-config" = {
+      visibility = "public"
+    }
+  }
+
+  # Sets the default of specific values if the object key is not set.
+  repositories = {
+    for k, v in local.repository_config : k => {
+      visibility = try(v.visibility, "private")
+    }
+  }
+}
+
+resource "github_repository" "core_cloud_repositories" {
+  for_each           = local.repositories
+  name               = each.key
+  visibility         = each.value.visibility
+  has_issues         = false
+  has_projects       = false
+  has_wiki           = false
+  has_downloads      = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true # TODO: Do we actually want to allow this?
+}

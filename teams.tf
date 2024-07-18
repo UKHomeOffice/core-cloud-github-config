f572686f1e116750af7bdfe88886729bd53e0b4f
Rob Johnson <robert.johnson@digital.homeoffice.gov.uk>
2024-07-18 12:52:05 +0100

    Reverting codeowner changes.  GitHub provider does not handle the ignoring of archived repos. https://github.com/integrations/terraform-provider-github/issues/737 These changes try to add additional to branch protection to archived repos which fails.

--- teams.tf
+++ teams.tf
@@ -5,8 +5,3 @@ data "github_team" "core_cloud_devops" {
 data "github_team" "core_cloud_admin" {
   slug = "core-cloud-admin"
 }
-
-resource "github_team" "core_cloud_code_owners" {
-  name           = "core-cloud-code-owners"
-  parent_team_id = data.github_team.core_cloud_devops.id
-}
